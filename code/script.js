4161996470a00e6b3c655597d1b9b31a0aa498f3
SETI108 <seti108@gmail.com>
2023-12-15 16:06:05 +0100

    Fixed multiple error logs

--- code/script.js
+++ code/script.js
@@ -2,6 +2,14 @@
 
 // Step 1 - Welcome and introduction
 // Your code goes here
+
+const errorAlert = () => {
+  alert(
+    "Thats's not a valid response. Please start over if you want to make and order"
+  );
+  location.reload();
+};
+
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 );
@@ -15,8 +23,7 @@ const firstName = prompt("Please enter your first name!");
 if (firstName) {
   alert(`Hi ${firstName}! Let's get started`);
 } else {
-  alert("Enter your name, starting over");
-  location.reload();
+  errorAlert();
 }
 
 // Step 2 - Food choice
@@ -44,15 +51,16 @@ if (firstName) {
 
 //Alert the user with the chosen food item
 
-if (foodChoiceOfUser > 3 || foodChoiceOfUser === 0) {
-  alert("Choose one of the options, starting over");
-  location.reload();
-} else {
-  alert(
-    `You've ordered ${foodChoices[foodChoiceOfUser - 1][0]} for a cost of ${
-      foodChoices[foodChoiceOfUser - 1][1]
-    } € `
-  );
+if (foodChoiceOfUser !== 0) {
+  if (foodChoiceOfUser > 3) {
+    errorAlert();
+  } else {
+    alert(
+      `You've ordered ${foodChoices[foodChoiceOfUser - 1][0]} for a cost of ${
+        foodChoices[foodChoiceOfUser - 1][1]
+      } € `
+    );
+  }
 }
 
 // Step 3 - Subtype choice
@@ -61,91 +69,85 @@ let topping;
 let toppingChoiceNumber;
 
 //Switch statement with handling of incorrent input
-
-switch (foodChoiceOfUser) {
-  case 0:
-    alert("Choose one of the options, starting over");
-    location.reload();
-    break;
-  case 1:
-    const pizzaToppings = ["Pineapple", "Banana", "Ketchup"];
-    toppingChoiceNumber = Number(
-      prompt(`
-      Choose your pizza toppings by writing the number corresponding to the topping you like:
-        - 1 Pineapple
-        - 2 Banana
-        - 3 Ketchup`)
-    );
-
-    if (toppingChoiceNumber < 4 && toppingChoiceNumber !== 0) {
-      alert(
-        `You've chosen ${foodChoices[foodChoiceOfUser - 1][0]} with ${
-          pizzaToppings[toppingChoiceNumber - 1]
-        }. `
+if (foodChoiceOfUser < 4 && foodChoiceOfUser !== 0) {
+  switch (foodChoiceOfUser) {
+    case 1:
+      const pizzaToppings = ["Pineapple", "Banana", "Ketchup"];
+      toppingChoiceNumber = Number(
+        prompt(`
+        Choose your pizza toppings by writing the number corresponding to the topping you like:
+          - 1 Pineapple
+          - 2 Banana
+          - 3 Ketchup`)
       );
-      topping = pizzaToppings[toppingChoiceNumber - 1];
-      break;
-    } else {
-      alert("Choose one of the options, starting over");
-      location.reload();
-      break;
-    }
 
-  case 2:
-    const pastaToppings = ["Olives", "Feta cheese", "Sun-dried tomatoes"];
-    toppingChoiceNumber = Number(
-      prompt(`
-    Choose your pasta toppings by writing the number corresponding to the topping you like:
-      - 1 Olives
-      - 2 Feta cheese
-      - 3 Sun-dried tomatoes
-    `)
-    );
+      if (toppingChoiceNumber < 4 && toppingChoiceNumber !== 0) {
+        alert(
+          `You've chosen ${foodChoices[foodChoiceOfUser - 1][0]} with ${
+            pizzaToppings[toppingChoiceNumber - 1]
+          }. `
+        );
+        topping = pizzaToppings[toppingChoiceNumber - 1];
+        break;
+      } else {
+        errorAlert();
 
-    if (toppingChoiceNumber < 4 && toppingChoiceNumber !== 0) {
-      alert(
-        `You've chosen ${foodChoices[foodChoiceOfUser - 1][0]} with ${
-          pastaToppings[toppingChoiceNumber - 1]
-        }. `
-      );
-      topping = pastaToppings[toppingChoiceNumber - 1];
-      break;
-    } else {
-      alert("Choose one of the options, starting over");
-      location.reload();
-      break;
-    }
+        break;
+      }
 
-  case 3:
-    const saladToppings = ["Grapes", "Avocado", "Red onion"];
-    toppingChoiceNumber = Number(
-      prompt(`
-    Choose your salad toppings by writing the number corresponding to the topping you like:
-    - 1 Grapes
-    - 2 Avocado
-    - 3 Red onion
+    case 2:
+      const pastaToppings = ["Olives", "Feta cheese", "Sun-dried tomatoes"];
+      toppingChoiceNumber = Number(
+        prompt(`
+      Choose your pasta toppings by writing the number corresponding to the topping you like:
+        - 1 Olives
+        - 2 Feta cheese
+        - 3 Sun-dried tomatoes
       `)
-    );
-    if (toppingChoiceNumber < 4 && toppingChoiceNumber !== 0) {
-      alert(
-        `You've chosen ${foodChoices[foodChoiceOfUser - 1][0]} with ${
-          saladToppings[toppingChoiceNumber - 1]
-        }. `
       );
-      topping = saladToppings[toppingChoiceNumber - 1];
-      break;
-    } else {
-      alert("Choose one of the options, starting over");
-      location.reload();
-      break;
-    }
 
-  default:
-    alert(
-      `That's not one of the choices! Please refresh the page to start again.`
-    );
+      if (toppingChoiceNumber < 4 && toppingChoiceNumber !== 0) {
+        alert(
+          `You've chosen ${foodChoices[foodChoiceOfUser - 1][0]} with ${
+            pastaToppings[toppingChoiceNumber - 1]
+          }. `
+        );
+        topping = pastaToppings[toppingChoiceNumber - 1];
+        break;
+      } else {
+        errorAlert();
+
+        break;
+      }
+
+    case 3:
+      const saladToppings = ["Grapes", "Avocado", "Red onion"];
+      toppingChoiceNumber = Number(
+        prompt(`
+      Choose your salad toppings by writing the number corresponding to the topping you like:
+      - 1 Grapes
+      - 2 Avocado
+      - 3 Red onion
+        `)
+      );
+      if (toppingChoiceNumber < 4 && toppingChoiceNumber !== 0) {
+        alert(
+          `You've chosen ${foodChoices[foodChoiceOfUser - 1][0]} with ${
+            saladToppings[toppingChoiceNumber - 1]
+          }. `
+        );
+        topping = saladToppings[toppingChoiceNumber - 1];
+        break;
+      } else {
+        errorAlert();
+
+        break;
+      }
+
+    default:
+      errorAlert();
+  }
 }
-
 // Step 4 - Age
 
 //Use the prompt to let the user enter their age
@@ -167,9 +169,6 @@ if (topping) {
         foodChoices[foodChoiceOfUser - 1][0]
       } with ${topping} for ${foodChoices[foodChoiceOfUser - 1][1]} €.`
     );
-  } else {
-    alert("Please enter an age as a number, starting over");
-    location.reload();
   }
 }
 
@@ -183,17 +182,16 @@ if (userAge > 0) {
       2 - Cancel order
     `)
   );
+} else if (userAge === 0) {
+  errorAlert();
 }
 
-if (confirmation !== 0) {
+if (confirmation) {
   if (confirmation === 1) {
     alert("Great, your order is being prepared!");
   } else if (confirmation === 2) {
     alert("Sorry to see you go. Start a new order by refreshing the page!");
   } else {
-    alert(
-      "Thats's not a valid response. Please start over if you want to make and order"
-    );
-    location.reload();
+    errorAlert();
   }
 }
